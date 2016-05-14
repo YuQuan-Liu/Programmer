99a617f425b1ac94adfa02290f6bc46711fd627e
rocket <rocket@84c58901-55bf-1047-979d-bdc90c5c95e7>
2016-05-14 01:42:21 +0000

    扫码写地址后   都表数时  使用readNum(raddr);
    git-svn-id: https://desktop-ee61sbk:8090/svn/RocketProgrammer/Programmer@50 84c58901-55bf-1047-979d-bdc90c5c95e7

--- src/com/rocket/programmer/window/Meter_NoEncrypt.java
+++ src/com/rocket/programmer/window/Meter_NoEncrypt.java
@@ -219,8 +219,12 @@ public class Meter_NoEncrypt extends JDialog {
 					raddr = Property.getStringValue("CSDM")+raddr;
 				}
 				
+				if(raddr.length()!= 14){
+					return;
+				}
+				
 				writeAddr(raddr);
-				readNum();
+				readNum(raddr);
 				addrTextField.setText("");
 			}
 		});
@@ -1142,6 +1146,95 @@ public class Meter_NoEncrypt extends JDialog {
 				e1.printStackTrace();
 			}
 		}
+	}
+	
+	public void readNum(String raddr) {
+		
+			// national
+			byte[] command = new byte[20];
+			
+			command[0] = (byte) 0xFE;
+			command[1] = (byte) 0xFE;
+			command[2] = (byte) 0xFE;
+			command[3] = (byte) 0xFE;
+			
+			command[4] = 0x68;
+			command[5] = 0x10;
+			
+			if(raddr.equals("")){
+				command[6] = (byte) 0xAA;
+				command[7] = (byte) 0xAA;
+				command[8] = (byte) 0xAA;
+				command[9] = (byte) 0xAA;
+				command[10] = (byte) 0xAA;
+				command[11] = (byte) 0xAA;
+				command[12] = (byte) 0xAA;
+			}else{
+				if(raddr.length()!=14  || !raddr.matches("[0-9]*")){
+					JOptionPane.showMessageDialog(panel_1, "输入地址长度不对！");
+					return;
+				}		
+				//meteraddr
+				byte[] maddr = StringUtil.string2Byte(raddr);
+				for(int i = 0;i < 7;i++){
+					command[6+i] = maddr[6-i];
+				}
+			}
+			
+			//control
+			command[13] = (byte) 0x01;
+			//length
+			command[14] = (byte) 0x03;
+			//data
+			command[15] = (byte) 0x1F;
+			command[16] = (byte) 0x90;
+			if(di1Checkbox.isSelected()){
+				command[15] = (byte) 0x90;
+				command[16] = (byte) 0x1F;
+			}
+			//serial 
+			command[17] = (byte) 0x01;
+			
+			command[18] = 0x00;
+			for(int i = 4;i < 18;i++){
+				command[18] += command[i];
+			}
+			command[19] = 0x16;
+			
+			try {
+				SerialWriter.queue_out.clear();
+				SerialReader.queue_in.clear();
+				SerialWriter.queue_out.put(command);
+				byte[] response = (byte[]) SerialReader.queue_in.poll(3, TimeUnit.SECONDS);
+				
+				if(response == null){
+					//超时
+					System.out.println("超时");
+					JOptionPane.showMessageDialog(panel_1, "超时");
+				}else{
+					System.out.println("response"+StringUtil.byteArrayToHexStr(response, response.length));
+					if(di1Checkbox.isSelected()){
+						if(response[12] == (byte)0x1F && response[11] == (byte)0x90){
+							int meterread = response[16]&0xFF;
+							meterread = meterread << 8;
+							meterread = meterread | response[15]&0xFF;
+							
+							showNumTextField.setText(Integer.toHexString(meterread));
+						}
+					}else{
+						if(response[11] == (byte)0x1F && response[12] == (byte)0x90){
+							int meterread = response[16]&0xFF;
+							meterread = meterread << 8;
+							meterread = meterread | response[15]&0xFF;
+							
+							showNumTextField.setText(Integer.toHexString(meterread));
+						}
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		
 	}
 	
 	public void writeAddr(String raddr){
